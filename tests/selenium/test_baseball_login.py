a1854c6229c8114118bb5044defcae69f30b267e
Julio Gastelum <tjuliogastelum90@gmail.com>
2018-12-12 22:11:30 -0800

    selenium test

---
+++ tests/selenium/test_baseball_login.py
@@ -0,0 +1,29 @@
+import unittest 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class BaseballLogin(unittest.TestCase):
+
+    def setUp(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        self.driver = webdriver.Chrome(options=options)
+
+    def test_login_baseball(self):
+        driver = self.driver
+        driver.get("http://localhost/front-end/")
+        self.assertIn("Login", driver.title)
+        user = driver.find_element_by_id("username")
+        user.send_keys("manuelObrador")
+        passw = driver.find_element_by_id("contra")
+        passw.send_keys("12345")
+        passw.send_keys(Keys.RETURN)
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
